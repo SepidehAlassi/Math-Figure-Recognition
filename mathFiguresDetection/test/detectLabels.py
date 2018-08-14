bug with scaling the image is fixed

mathFiguresDetection/test/detectLabels.py
@@ -13,6 +13,7 @@ from keras_retinanet.preprocessing.csv_generator import CSVGenerator
 
 import tensorflow as tf
 
+
 def get_session():
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
@@ -22,7 +23,7 @@ def parse_args():
     parser = argparse.ArgumentParser(description='testing script.')
     parser.add_argument('--annotations', default='train/annotations.csv', help='Path to annotations')
     parser.add_argument('--classes', default='train/meditatio_classes.csv', help='Path to a CSV file containing class label mapping (required)')
-    parser.add_argument('--val_path', default='test/images/Med_Ms_p0132_F_1_rgb_extracted.jpg', help='Path to image')
+    parser.add_argument('--val_path', default='test/images/Med_Ms_p0132_M_5_rgb_extracted.jpg', help='Path to image')
     parser.add_argument('--weights', help='Weights to use for initialization (defaults to ImageNet).',
                         default='imagenet')
     parser.add_argument('--batch-size', help='Size of the batches.', default=1, type=int)
@@ -30,20 +31,22 @@ def parse_args():
 
     return parser.parse_args()
 
+
 def getCoord(b, size, scale):
     mylist=[]
     for coord in b:
         if coord>size:
             mylist.append(coord)
-    # if len(mylist):
-    #     scaled = [int(x) for x in b / scale]
-    #     return np.asarray(scaled)
-    # else:
-    return b
+    if len(mylist):
+        scaled = [int(x) for x in b / scale]
+        return np.asarray(scaled)
+    else:
+        return b
 
-if __name__ == '__main__':
-    # parse arguments
+
+def detectAlphabets(imageToRecognize):
     args = parse_args()
+    args.val_path = imageToRecognize
     # os.environ["CUDA_VISIBLE_DEVICES"] = "0"
     keras.backend.tensorflow_backend.set_session(get_session())
     model = keras.models.load_model('../snapshots/resnet50_csv_wtext.h5', custom_objects=custom_objects)
@@ -66,49 +69,45 @@ if __name__ == '__main__':
 
     # preprocess image for network
     image = preprocess_image(image)
-    image, scale =resize_image(image)
-
-
+    image, scale = resize_image(image)
 
     # process image
     start = time.time()
     _, _, detections = model.predict_on_batch(np.expand_dims(image, axis=0))
     print("processing time: ", time.time() - start)
-    print('detections:',detections)
+    print('detections:', detections)
     # compute predicted labels and scores
     predicted_labels = np.argmax(detections[0, :, 4:], axis=1)
     scores = detections[0, np.arange(detections.shape[1]), 4 + predicted_labels]
-    print("label=",predicted_labels)
+    print("label=", predicted_labels)
     # correct for image scale
-    detections[:, :4] /= scale
+    scaled_detection = detections[0, :, :4] / scale
 
     # visualize detections
-    repeated = 0
     recognized = {}
-    recognized_newscale = {}
+
+    plt.figure(figsize=(15, 15))
+    plt.axis('off')
     for idx, (label, score) in enumerate(zip(predicted_labels, scores)):
         if score < 0.4:
             continue
-        b = detections[0, idx, :4].astype(int)
+        b = scaled_detection[idx, :4].astype(int)
+        cv2.rectangle(draw, (b[0], b[1]), (b[2], b[3]), (0, 0, 255), 1)
 
-        label = test_generator.label_to_name(label)
-        caption = "{} {:.1f}".format(label, score)
-        if label in recognized.keys():
-            label = caption+"_"+str(repeated)
-            repeated += 1
-        scaled = getCoord(b, len(draw), scale)
+        caption = test_generator.label_to_name(label)
+        cv2.putText(draw, caption, (b[0], b[1] - 1), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1)
+        recognized[caption] = b
+        print(caption + ":" + str(score))
 
-        recognized[label] = scaled
-
-    # cv2.putText(draw, caption, (b[0], b[1]-1), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1)
-    print(recognized)
-
-    for key, value in recognized.items():
-        cv2.rectangle(draw, (value[0], value[1]), (value[2], value[3]), (0, 0, 255), 1)
-        cv2.putText(draw, key, (value[0], value[1] - 1), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1)
-    plt.figure(figsize=(15, 15))
-    plt.axis('off')
     plt.imshow(draw)
     plt.show()
+    return recognized, draw
+if __name__ == '__main__':
+    # parse arguments
+
+    imageToRecognize = os.path.join('test/images', 'Med_MS_p0165_F_1_rgb_extracted.jpg')
+    recognized, image = detectAlphabets(imageToRecognize)
+
+
 
 